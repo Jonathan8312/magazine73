1534923f797d99aaa39d1f287f677ecba03889b4
JT <jonathan8312@gmail.com>
2026-07-10 18:21:10 -0400

    first commit

---
+++ plugin/magazine73/magazine73.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Magazine73
+ * Plugin URI: https://73software.com/magazine73
+ * Description: Permite administrar y publicar revistas digitales con efecto de cambio de página.
+ * Version: 0.1.0
+ * Requires at least: 6.7
+ * Requires PHP: 8.1
+ * Author: Jonathan Torres
+ * Author URI: https://73software.com
+ * License: GPL-2.0-or-later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: magazine73
+ * Domain Path: /languages
+ */
+
+defined('ABSPATH') || exit;
